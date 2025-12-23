9fd63360cbe6fced426f04a620545fb0e6552991
Michael Pyle <mpyle101@gmail.com>
2025-12-22 18:07:24 -0700

    Add command line argument for script file

--- crates/anvil-cli/src/main.rs
+++ crates/anvil-cli/src/main.rs
@@ -1,13 +1,22 @@
+use anyhow::Result;
+use clap::Parser;
 use anvil_runtime::Interpreter;
 
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()>
-{
-    let mut interpreter = Interpreter::default();
+#[derive(Parser)]
+struct Cli {
+    /// Path to the Anvil script to run
+    script: String,
+}
 
-    let source = std::fs::read_to_string("./scripts/join.anvil")?;
+#[tokio::main]
+async fn main() -> Result<()>
+{
+    let cli = Cli::parse();
+    let source = std::fs::read_to_string(&cli.script)?;
     let program = anvil_parse::parser::parse_program(&source)?;
+
+    let mut interpreter = Interpreter::default();
     interpreter.eval(program).await?;
 
     Ok(())
